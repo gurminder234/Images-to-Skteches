1e6823c6c7b897e10dc592f202294c4ee99594c0
Gurminder Singh <83048956+gurminder234@users.noreply.github.com>
2021-09-18 19:37:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import cv2
-img = cv2.imread('1.png')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+import cv2 #pip install "opencv-python"
+img = cv2.imread('Name of your image.format') #The image should be in same folder as this code
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) 
 invert = 255 - gray
 blur = cv2.GaussianBlur(invert,(21,21),0)
 inverted = 255 - blur
